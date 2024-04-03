fc8aa0e8c77d7e8b91477d069303910696dc2042
yutama <tamcrush@yahoo.com>
2024-04-04 09:40:13 +1100

    db setup
    DB setup is completed
    adding mongodb, json

---
+++ backend/controllers/user.js
@@ -0,0 +1,6 @@
+exports.home = (req, res) => {
+  res.status(200).json({
+    message: "adsfsdjfnasdojb",
+    lala: "asdfasdf",
+  });
+};
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const { readdirSync } = require("fs");
+const dotenv = require("dotenv");
 
 const app = express();
+app.use(express.json());
+dotenv.config();
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("welcome from home");
-});
-app.get("/books", (req, res) => {
-  res.send("hahahahahahahhahahaaiidhiagduogauodhguagdigaiduygiuagduagdiu");
-});
-app.listen(8000, () => {
-  console.log("server is lestining...");
+//ROUTES
+readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
+
+//DB
+mongoose
+  .connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+  .then(() => console.log("DB Connect Success"))
+  .catch((err) => console.log("DB Connection error", err));
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`server is runing on port ${PORT}...`);
 });
