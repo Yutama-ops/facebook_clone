a064dd8587f90d29d0629c1812f9e613c194bdcc
yutama <tamcrush@yahoo.com>
2024-04-05 11:54:05 +1100

    login
    finalise login controller with validation

---
+++ backend/helpers/mailer.js
@@ -0,0 +1,57 @@
+const nodemailer = require("nodemailer");
+
+const { google } = require("googleapis");
+
+const { OAuth2 } = google.auth;
+const oauth_link = "https://developers.google.com/oauthplayground";
+const { EMAIL, MAILING_ID, MAILING_REFRESH, MAILING_SECRET } = process.env;
+
+const auth = new OAuth2(
+  MAILING_ID,
+  MAILING_SECRET,
+  MAILING_REFRESH,
+  oauth_link
+);
+
+exports.sendVerificationEmail = (email, name, url) => {
+  auth.setCredentials({
+    refresh_token: MAILING_REFRESH,
+  });
+  const accessToken = auth.getAccessToken();
+  const stmp = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: EMAIL,
+      clientId: MAILING_ID,
+      clientSecret: MAILING_SECRET,
+      refreshToken: MAILING_REFRESH,
+      accessToken,
+    },
+  });
+  const mailOption = {
+    from: EMAIL,
+    to: email,
+    subject: "Mock Facebook email verification",
+    html: `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+      </head>
+      <body>
+        <div>
+          <img src="" alt="" />
+          <span> Action require: Activate your account </span>
+          <a href="${url}"> Confirm your account </a>
+        </div>
+      </body>
+    </html>
+    `,
+  };
+  stmp.sendMail(mailOption, (err, res) => {
+    if (err) return err;
+    return res;
+  });
+};
